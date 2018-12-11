0ddec94c149149092bd84602a4f581fd043ca591
KarthusLorin <karthuslorin@gmail.com>
2018-12-11 21:58:57 +0800

    add heap sift up

--- Heap/heap.js
+++ Heap/heap.js
@@ -26,13 +26,30 @@ class MaxHeap {
     return (index - 1) / 2
   }
 
-  // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子的索引
+  // 返回完全二叉树的数组表示中，索引为index的左节点的总索引
   leftChild (index) {
     return index * 2 + 1
   }
 
-  // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子的索引
+  // 返回完全二叉树的数组表示中，索引为index的右节点的总索引
   rightChild (index) {
     return index * 2 + 2
+  }
+
+  // 向堆中添加元素
+  add (e) {
+    this.data.push(e)
+    this.siftUp(this.data.length - 1)
+  }
+
+  // 目标元素上浮
+  siftUp (k) {
+    while (k > 0 && this.data[this.parent(k)] < this.data[k]) {
+      // 交换父元素和k的位置
+      let temp = this.data[this.parent(k)]
+      this.data[this.parent(k)] = this.data[k]
+      this.data[k] = temp
+      k = this.parent(k)
+    }
   }
 }
