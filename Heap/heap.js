79a5d59b43bd3dba7bb1c4564a111c68c3d0465f
KarthusLorin <karthuslorin@gmail.com>
2018-12-12 21:31:15 +0800

    add

--- Heap/heap.js
+++ Heap/heap.js
@@ -46,10 +46,57 @@ class MaxHeap {
   siftUp (k) {
     while (k > 0 && this.data[this.parent(k)] < this.data[k]) {
       // 交换父元素和k的位置
-      let temp = this.data[this.parent(k)]
-      this.data[this.parent(k)] = this.data[k]
-      this.data[k] = temp
+      this.swap(this.parent(k), k)
       k = this.parent(k)
     }
+  }
+
+  // 看堆中的最大元素
+  findMax () {
+    return this.data[0]
+  }
+
+  // 取出堆中最大元素
+  extractMax () {
+    let ret = this.findMax()
+
+    this.swap(0, this.data.length - 1)
+    // 取出末尾
+    this.data.length--
+    this.siftDown(0)
+
+    return ret
+  }
+
+  // 目标元素下沉
+  siftDown(k) {
+    while (this.leftChild(k) < this.data.length) {
+      let j = this.leftChild(k)
+      if (j + 1 < this.data.length && this.data[j + 1] > this.data[j]) {
+        j = this.rightChild(k)
+      }
+
+      if (this.data[k] >= this.data[j]) {
+        break
+      }
+
+      this.swap(k, j)
+      k = j
+    }
+  }
+
+  swap (a, b) {
+    let temp = this.data[a]
+    this.data[a] = this.data[b]
+    this.data[b] = temp
+  }
+
+  // 取出堆中的最大元素，并且替换成元素e
+  replace (e) {
+    let ret = this.findMax()
+
+    this.data.set()
+
+    return ret
   }
 }
